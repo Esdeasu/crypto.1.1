1bfe0556a292d926e3eaeaef31a45152e8030d19
ak1lucky <senya.gioco@mail.ru>
2023-02-22 18:42:49 +0300

    Practice with vue3, first separating on components

---
+++ src/api.js
@@ -0,0 +1,69 @@
+const API_KEY =
+  "f443e0447aa9c7a286bd23c279eade013b7ff8712d1b494ca665edc0004c44dd";
+
+const tickersHandlers = new Map(); // {}
+const socket = new WebSocket(
+  `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
+);
+
+const AGGREGATE_INDEX = "5";
+
+socket.addEventListener("message", (e) => {
+  const {
+    TYPE: type,
+    FROMSYMBOL: currency,
+    PRICE: newPrice,
+    //INFO: info,
+    //MESSAGE: INVALID_SUB,
+    //PARAMETER: params,
+  } = JSON.parse(e.data);
+  if (type !== AGGREGATE_INDEX || newPrice === undefined) {
+    return;
+  }
+
+  const handlers = tickersHandlers.get(currency) ?? [];
+  handlers.forEach((fn) => fn(newPrice));
+});
+
+function sendToWebSocket(message) {
+  const stringifiedMessage = JSON.stringify(message);
+
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(stringifiedMessage);
+    return;
+  }
+
+  socket.addEventListener(
+    "open",
+    () => {
+      socket.send(stringifiedMessage);
+    },
+    { once: true }
+  );
+}
+
+
+function subscribeToTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubAdd",
+    subs: [`5~CCCAGG~${ticker}~USD`, `0~CCCAGG~${ticker}~BTC`],
+  });
+}
+
+function unsubscribeFromTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubRemove",
+    subs: [`5~CCCAGG~${ticker}~USD`, `0~CCCAGG~${ticker}~BTC`],
+  });
+}
+
+export const subscribeToTicker = (ticker, cb) => {
+  const subscribers = tickersHandlers.get(ticker) || [];
+  tickersHandlers.set(ticker, [...subscribers, cb]);
+  subscribeToTickerOnWs(ticker);
+};
+
+export const unsubscribeFromTicker = (ticker) => {
+  tickersHandlers.delete(ticker);
+  unsubscribeFromTickerOnWs(ticker);
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/* eslint-env node */
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
